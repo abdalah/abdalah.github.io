347a8e44033443830349e84f9ee98772b80f8420
El-Barrad, Abdalah <Abdalah.El-Barrad@adidas.com>
2019-11-21 23:26:21 -0800

    added easter egg and tabs

---
+++ js/background_change.js
@@ -0,0 +1,5 @@
+function background_change(){
+    document.getElementById("title").addEventListener("click", function(){
+      document.getElementById("tracksuit_background").id = "young_background";
+    });
+}
